b752ffcc9c325249a3d478aeb9b7500789fd9401
Michael Pickett <micha@MichaelPC.localdomain>
2026-08-30 12:34:50 -0600

    remembering im in python

--- age.py
+++ age.py
@@ -1,14 +1,12 @@
+from random import randint
 print("Hello, I'm going to guess your age. ")
 name = input("What is your name? ")
-while (True) {
-    guess = random(15,40)
-    printf("Are you {guess} years old? ")
+while (True):
+    guess = randint(15,40)
+    print(f"Are you {guess} years old? ")
     answer = input()
-    if (answer == "y") {
-        printf("{name} is {guess} years old.")
-        exit
-    }
-    else {
+    if (answer == "y"):
+        print(f"{name} is {guess} years old.")
+        exit(0)
+    else:
         print("Rats")
-    }
-}
